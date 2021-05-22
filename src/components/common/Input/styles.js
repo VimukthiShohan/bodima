0afd38ed5be76e76286a98edb6d2ab18826869b9
VSLuci <vimukthi.shohan@users.noreply.github.com>
2021-05-23 00:01:34 +0530

    Adding text input fields

---
+++ src/components/common/Input/styles.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+import colors from '../../../assets/theme/colors';
+
+export default StyleSheet.create({
+  wrapper: {
+    height: 42,
+    borderWidth: 2,
+    borderRadius: 10,
+    paddingHorizontal: 1,
+    marginTop: 5,
+  },
+  textInput: {
+    flex: 1,
+    color: colors.grey,
+  },
+  inputContainer: {
+    paddingVertical: 12,
+  },
+  error: {
+    color: colors.danger,
+    paddingTop: 4,
+  },
+});
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
 import Container from '../../components/common/Container';
+import Input from '../../components/common/Input';
+import {Text} from 'react-native';
 
 const Login = () => {
+  const [text, onChangeText] = useState('');
+
   return (
     <Container>
-      <Text>Hello from Login</Text>
+      <Input
+        label="Username"
+        onChangeText={onChangeText}
+        value={text}
+        iconPosition="right"
+        // error={'This field is required'}
+      />
+      <Input
+        label="Password"
+        onChangeText={onChangeText}
+        value={text}
+        icon={<Text>HIDE</Text>}
+        iconPosition="right"
+      />
     </Container>
   );
 };
